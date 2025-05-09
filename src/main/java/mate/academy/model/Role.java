9423ae4756e963a1d245cd2b5f26abb3265ac3fe
Olha <olha.oleshchuk@gmail.com>
2025-05-09 16:33:28 +0300

    add security issues

---
+++ src/main/java/mate/academy/model/Role.java
@@ -0,0 +1,30 @@
+package mate.academy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String roleName;
+
+    public enum RoleName {
+        ADMIN,
+        USER
+    }
+}
---
+++ src/main/java/mate/academy/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package mate.academy.security;
+
+import lombok.RequiredArgsConstructor;
+import mate.academy.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+}
