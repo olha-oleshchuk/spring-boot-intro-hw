826f6d9578cb33f0c8339af0fc578a00771ce07f
Olha <olha.oleshchuk@gmail.com>
2025-05-15 11:54:41 +0300

    done liquibase remarks

--- src/main/java/mate/academy/dao/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dao/CreateBookRequestDto.java
@@ -20,5 +20,6 @@ public class CreateBookRequestDto {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @NotNull
     private Set<Long> categoryIds;
 }
