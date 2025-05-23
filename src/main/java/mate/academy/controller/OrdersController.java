7eb2808e2cf5160331d001968b3336f9e8128d36
Olha <olha.oleshchuk@gmail.com>
2025-05-23 09:38:57 +0300

    few changes

--- src/main/java/mate/academy/controller/OrdersController.java
+++ src/main/java/mate/academy/controller/OrdersController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,7 +34,7 @@ public class OrdersController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrderResponseDto createOrder(
-            @RequestParam Long userId,
+            @PathVariable Long userId,
             @RequestBody @Valid OrderRequestDto requestDto) {
         return orderService.createOrder(userId, requestDto);
     }
